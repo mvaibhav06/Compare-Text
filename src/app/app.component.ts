1e0d01b215ab1b025ca7f8c9c7f312f8df07142c
vaibhav <vaibhavmasetty06@gmail.com>
2024-03-08 14:26:39 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence();
+  didUserWon = false;
+  enteredText = '';
+
+  onInput(val) {
+    this.enteredText = val;
+  }
+
+  getClass(randomLetter: string, enteredLetter: String) {
+    if (!enteredLetter) {
+      return 'pending';
+    } else if (enteredLetter === randomLetter) {
+      return 'correct';
+    } else {
+      return 'incorrect';
+    }
+  }
 }
